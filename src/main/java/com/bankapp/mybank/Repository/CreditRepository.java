cc9112a1f49b9241490a46ffdf82dab236b697be
romankuryshev <romankuryshev1406@gmail.com>
2023-05-12 01:08:44 +0300

    Добавлена поддержка операций с кредитами

--- src/main/java/com/bankapp/mybank/Repository/CreditRepository.java
+++ src/main/java/com/bankapp/mybank/Repository/CreditRepository.java
@@ -1,8 +1,14 @@
 package com.bankapp.mybank.Repository;
 
 import com.bankapp.mybank.Model.Credit;
+import com.bankapp.mybank.Model.DebitCard;
+import com.bankapp.mybank.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CreditRepository extends JpaRepository<Credit, Long> {
     public Credit findCreditByCredId(Long credId);
+    public List<Credit> findAllCreditsByUser(User user);
+    Credit findCreditByDebitCard(DebitCard card);
 }
